0042843c09423126f5b2c1d0dca5f97c215c3c18
Daniel Roth <daroth@microsoft.com>
2023-09-18 21:49:22 -0700

    Update to .NET 8 RC1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazorSections.Data;
+using BlazorSections.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRazorComponents()
+    .AddServerComponents();
 
 var app = builder.Build();
 
@@ -23,9 +20,7 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+    .AddServerRenderMode();
 
 app.Run();
